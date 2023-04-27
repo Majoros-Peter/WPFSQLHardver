79faad79a98a2805eba7f91557582f053f7f9ece
Majoros-Peter <70958388+Majoros-Peter@users.noreply.github.com>
2023-04-27 13:32:52 +0200

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private const string kapcsolatLeiro = "datasource=127.0.0.1;port=3306;username=root;password=;database=hardver;charset=utf8";
+    ObservableCollection<Termek> termekek = new();
+    MySqlConnection SQLkapcsolat;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        dgTermekek.ItemsSource = termekek;
+
+        AdatbazisMegnyitas();
+        KategoriakBetoltese();
+        GyartokBetoltese();
+
+        TermekekBetolteseListaba();
+
+        AdatbazisLezarasa();
+    }
+
+    private void btnMentes_Click(object sender, RoutedEventArgs e)
+    {
+        AdatbazisMegnyitas();
+        StreamWriter sw = new("blabla.csv");
+
+        foreach(Termek termek in termekek)
+        {
+            sw.WriteLine(termek.ToCSVString());
+        }
+        sw.Close();
+    }
+
+    private void btnSzukit_Click(object sender, RoutedEventArgs e)
+    {
+        AdatbazisMegnyitas();
+        termekek.Clear();
+        string SQLSzukitettLista = SzukitoLekerdezesEloallitasa();
+        MySqlCommand SQLparancs = new(SQLSzukitettLista, SQLkapcsolat);
+        MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+
+        while (eredmenyOlvaso.Read())
+            termekek.Add(new(eredmenyOlvaso));
+
+        eredmenyOlvaso.Close();
+    }
+
+    private void TermekekBetolteseListaba()
+    {
+        string SQLOsszesTermek = "SELECT * FROM termékek;";
+        MySqlCommand SQLparancs = new(SQLOsszesTermek, SQLkapcsolat);
+        MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+
+        while (eredmenyOlvaso.Read())
+            termekek.Add(new(eredmenyOlvaso));
+
+        eredmenyOlvaso.Close();
+    }
+
+    private void KategoriakBetoltese()
+    {
+        string SQLKategoriakRendezve = "SELECT DISTINCT kategória FROM termékek ORDER BY 1;";
+        MySqlCommand SQLparancs = new(SQLKategoriakRendezve, SQLkapcsolat);
+        MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+
+        cbKategoria.Items.Add(" - Nincs megadva - ");
+        while (eredmenyOlvaso.Read())
+            cbKategoria.Items.Add(eredmenyOlvaso.GetString("Kategória"));
+
+        eredmenyOlvaso.Close();
+        cbKategoria.SelectedIndex = 0;
+    }
+
+    private void GyartokBetoltese()
+    {
+        string SQLKategoriakRendezve = "SELECT DISTINCT gyártó FROM termékek ORDER BY 1;";
+        MySqlCommand SQLparancs = new(SQLKategoriakRendezve, SQLkapcsolat);
+        MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+
+        cbGyarto.Items.Add(" - Nincs megadva - ");
+        while (eredmenyOlvaso.Read())
+            cbGyarto.Items.Add(eredmenyOlvaso.GetString("Gyártó"));
+
+        eredmenyOlvaso.Close();
+        cbGyarto.SelectedIndex = 0;
+    }
+
+    private void AdatbazisMegnyitas()
+    {
+        try
+        {
+            SQLkapcsolat = new(kapcsolatLeiro);
+            SQLkapcsolat.Open();
+        }
+        catch(Exception e)
+        {
+            MessageBox.Show(e.Message);
+            this.Close();
+        }
+    }
+
+    private void AdatbazisLezarasa()
+    {
+        SQLkapcsolat.Close();
+        SQLkapcsolat.Dispose();
+    }
+
+    private string SzukitoLekerdezesEloallitasa() => $"SELECT * FROM termékek WHERE { (cbKategoria.SelectedIndex > 0 ? $"Kategória LIKE '{cbKategoria.SelectedValue}'" : "true") } and {(cbGyarto.SelectedIndex > 0 ? $"Gyártó LIKE '{cbGyarto.SelectedValue}'" : "true")};";
+}
---
+++ WpfApp1/Termek.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp1
+{
+    public class Termek
+    {
+        string kategoria;
+        string gyarto;
+        string nev;
+        int ar;
+        int garido;
+
+        public Termek(string kategoria, string gyarto, string nev, int ar, int garido)
+        {
+            this.kategoria = kategoria;
+            this.gyarto = gyarto;
+            this.nev = nev;
+            this.ar = ar;
+            this.garido = garido;
+        }
+
+        public Termek(MySqlDataReader olvaso)
+        {
+            this.kategoria = olvaso.GetString("Kategória");
+            this.gyarto = olvaso.GetString("Gyártó");
+            this.nev = olvaso.GetString("Név");
+            this.ar = olvaso.GetInt32("Ár");
+            this.garido = olvaso.GetInt32("Garidő");
+        }
+
+        public string Kategoria { get => kategoria; set => kategoria = value; }
+        public string Gyarto { get => gyarto; set => gyarto = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public int Ar { get => ar; set => ar = value; }
+        public int Garido { get => garido; set => garido = value; }
+
+        public string ToCSVString() => $"{kategoria};{gyarto};{nev};{ar};{garido}";
+    }
+}
